78714938c570c19941700e0452e7f6b72c6ebe43
10fu3 <msf.1192@gmail.com>
2021-08-18 06:37:39 +0900

    Repositoryに求められる引数と返り値をもったメソッドを定義

--- repository/service.go
+++ repository/service.go
@@ -1,8 +1,11 @@
 package repository
 
+import "toufu3.jp/Idp/model"
+
 type IServiceRepository interface {
-	CreateService()
-	UpdateService()
-	GetService(uuid string)
+	CreateService(s model.IService)
+	UpdateService(s model.IService)
+	GetService(uuid string) model.IService
+	GetAllServices() []model.IService
 	DeleteService(uuid string)
 }
