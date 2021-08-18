bb6298051f588f604de4442e42a3517f8f897f31
10fu3 <msf.1192@gmail.com>
2021-08-18 17:39:59 +0900

    検索時に1件もヒットしない場合はnilを返す実装に変更

--- repository/account_impl.go
+++ repository/account_impl.go
@@ -7,7 +7,7 @@ import (
 
 func (r *AllDB) CreateAccountByTempAccount(account model.ITempAccount) error {
 
-	if r.GetAccountByMail(account.GetMail()) != nil{
+	if r.GetAccountByMail(account.GetMail()) != nil {
 		return fmt.Errorf("exist mail address")
 	}
 
@@ -16,7 +16,7 @@ func (r *AllDB) CreateAccountByTempAccount(account model.ITempAccount) error {
 }
 
 func (r *AllDB) CreateAccount(account model.IAccount) error {
-	if r.GetAccountByMail(account.GetMail()) != nil{
+	if r.GetAccountByMail(account.GetMail()) != nil {
 		return fmt.Errorf("exist mail address")
 	}
 	return r.db.Create(account).Error
@@ -24,20 +24,28 @@ func (r *AllDB) CreateAccount(account model.IAccount) error {
 
 func (r *AllDB) GetAccount(uuid string) model.IAccount {
 	var a model.Account
-	r.db.Select("uuid = ?",uuid).Find(&a)
+	var c int64
+	r.db.Select("uuid = ?", uuid).Find(&a).Count(&c)
+	if c == 0 {
+		return nil
+	}
 	return &a
 }
 
 func (r *AllDB) GetAccountByMail(mail string) model.IAccount {
 	var a model.Account
-	r.db.Select("mail = ?",mail).Find(&a)
+	var c int64
+	r.db.Select("mail = ?", mail).Find(&a).Count(&c)
+	if c == 0 {
+		return nil
+	}
 	return &a
 }
 
 func (r *AllDB) GetAllAccounts() []model.IAccount {
 	var a []model.Account
 	r.db.Find(&a)
-	var b = make([]model.IAccount,5)
+	var b = make([]model.IAccount, 5)
 
 	for _, v := range a {
 		b = append(b, &v)
@@ -47,9 +55,9 @@ func (r *AllDB) GetAllAccounts() []model.IAccount {
 }
 
 func (r *AllDB) UpdateAccount(account model.IAccount) error {
-	return r.db.Save(*account.(*model.Account)).Error
+	return r.db.Where("uuid = ?", account.GetUUID()).UpdateColumns(account).Error
 }
 
 func (r *AllDB) DeleteAccount(uuid string) {
-	r.db.Unscoped().Delete(model.Account{},"uuid = ?",uuid)
+	r.db.Unscoped().Delete(model.Account{}, "uuid = ?", uuid)
 }
